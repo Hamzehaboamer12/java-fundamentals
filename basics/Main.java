5fe91ccb9afcc074b0b60d131b29b99e0d983930
Hamzehaboamer12 <hamzehaboamee@gmail.com>
2022-02-27 21:02:06 +0200

    edit

--- basics/Main.java
+++ basics/Main.java
@@ -16,50 +16,56 @@ class Main {
 
         int turtleCount = 0;
         System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
-      flipHeads(6);
+        flipHeads(2);
         clock();
 
     }
-    public static void clock(){
-        while (true){
+
+    public static void clock() {
+        while (true) {
             LocalDateTime now = LocalDateTime.now();
             int hour = now.getHour();
             int minute = now.getMinute();
             int second = now.getSecond();
             String time = now.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
-            while (second==LocalDateTime.now().getSecond()){
+            while (second == LocalDateTime.now().getSecond()) {
                 continue;
             }
             System.out.println(time);
-        }}
+        }
+    }
 
-        public static String pluralize (String name,int count) {
-            if (count == 1) {
-                return name;
+    public static String pluralize(String name, int count) {
+        if (count == 1) {
+            return name;
+        } else {
+            return name + "s";
+        }
+    }
+
+    public static void flipHeads(int n) {
+        int counter = 0;
+        int headsCounter = 0;
+
+        while (headsCounter < n) {
+            Random random = new Random();
+            if (random.nextDouble() < 0.5) {
+
+                counter++;
+                headsCounter = 0;
+                System.out.println("tail");
+
             } else {
-                return name + "s";
-            }
-        }
-    public static void flipHeads (int n){
-        Random random = new Random();
-        int counter=0 , hFlips=0 ,tFlips=0;
-        while(counter<n)
-        {
-            double randomNum = random.nextDouble();
-            if(randomNum<0.5)
-            {
-                System.out.println("tails");
-                tFlips++;
                 counter++;
-            }
-            else
-            {
+                headsCounter++;
                 System.out.println("heads");
-                counter++;
-                hFlips++;
+
             }
+
         }
-        System.out.println("It took "+n+" flips to flip "+hFlips+" head in a row.");
+
+        System.out.println("It took " + counter + " flips to flip " + n + " heads in a row.");
+
     }
 }
 
