4d682dde72049f5e06d959fe2d5476fc138d5712
Hamzehaboamer12 <hamzehaboamee@gmail.com>
2022-03-06 19:53:40 +0200

    add res

---
+++ inheritance/lib/src/main/java/inheritance/Restaurant.java
@@ -0,0 +1,78 @@
+package inheritance;
+
+import java.util.ArrayList;
+
+public class Restaurant {
+
+     String name;
+     String price;
+     ArrayList<String> reviews = new ArrayList<>();
+     ArrayList<Integer> starsArray = new ArrayList<>();
+     int totalStars;
+
+    // constructor
+    public Restaurant(String name , String price) {
+        this.name = name;
+        this.price = price;
+
+    }
+
+
+    private void calculateTotalStars() {
+        int totalSum = 0;
+        for (Integer item : starsArray) {
+            totalSum = totalSum + item;
+        }
+        this.totalStars = totalSum / starsArray.size();
+    }
+
+    public String addReview(Review review) {
+        if (review.getStars() < 0 || review.getStars() > 5) {
+            return "Please rating must be between 0 and 5 stars";
+        } else {
+            String userReview = "Reviewer : " + review.getAuthor() + "\n" + "Review : " + review.getBody() + "\n" + "Rating :  " + review.getStars() + " stars" + "\n";
+
+            setReviews(userReview);
+            setStarsArray(review.getStars());
+
+            this.calculateTotalStars();
+            return null;
+        }
+
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTotalStars() {
+        return totalStars;
+    }
+
+    public ArrayList<String> getReviews() {
+        return reviews;
+    }
+
+
+    public void setReviews(String review) {
+        this.reviews.add(review);
+    }
+
+    public void setStarsArray(int stars) {
+        this.starsArray.add(stars);
+    }
+
+    @Override
+    public String toString() {
+
+        return "\nRestaurant : " + name + '\n' +
+                "Price  : " + price + '\n' +
+                "Stars : " + totalStars + '\n' + "\n" +
+                "Reviews : \n" + reviews;
+    }
+
+
+
+}
---
+++ inheritance/lib/src/main/java/inheritance/Review.java
@@ -0,0 +1,48 @@
+package inheritance;
+
+public class Review {
+    String body = null;
+    String author = null;
+    int stars = 0;
+
+
+    public Review(String body , String author , int stars) {
+        this.body = body;
+        this.author = author ;
+        this.stars = stars ;
+    }
+
+    @Override
+    public String toString() {
+        return "Review" +
+                "body='" + body + '\'' +
+                ", author='" + author + '\'' +
+                ", stars=" + stars;
+    }
+
+
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setStars(int stars) {
+        this.stars = stars;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+}
